c41086c229a567523910b6a894c83a91187cfbd1
Kdrouin <drouin.kaourantin@gmail.com>
2025-01-21 11:17:30 +0100

    all

---
+++ get_next_line.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadrouin <kadrouin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 10:22:48 by kadrouin          #+#    #+#             */
+/*   Updated: 2025/01/21 10:24:42 by kadrouin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	buffer;
+	char	line[7000000];
+	int		b;
+	int		i;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	i = 0;
+	b = read(fd, &buffer, 1);
+	while (b > 0)
+	{
+		line[i++] = buffer;
+		if (buffer == '\n')
+			break ;
+		b = read(fd, &buffer, 1);
+	}
+	line[i] = '\0';
+	if (b <= 0 && i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
---
+++ nl
@@ -0,0 +1,5 @@
+This is the first line.
+Second line is here.
+Third line.
+Fourth.
+Fifth line is the longest one in this file.
